4af65a360f2e824997b03d24c9eac21f51dfaeab
irooit <hi@rooit.com>
2018-11-11 16:41:22 +0800

    修改数据库

--- sql/user_level.sql
+++ sql/user_level.sql
@@ -4,8 +4,14 @@
  Source Server         : 亚马逊美西
  Source Server Type    : MySQL
  Source Server Version : 50641
+ Source Host           : rooit.c6xbwtcnposp.us-west-1.rds.amazonaws.com:3306
+ Source Schema         : shadow
 
- Date: 11/11/2018 16:34:37
+ Target Server Type    : MySQL
+ Target Server Version : 50641
+ File Encoding         : 65001
+
+ Date: 11/11/2018 16:40:54
 */
 
 SET NAMES utf8mb4;
@@ -21,5 +27,13 @@ CREATE TABLE `user_level` (
   `name` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_level
+-- ----------------------------
+BEGIN;
+INSERT INTO `user_level` VALUES (1, 0, 'User');
+INSERT INTO `user_level` VALUES (2, 1, 'Admin');
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
